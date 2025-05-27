35f624d01bde8d847a9e41d362cedc69b6213fb2
mohamad1Aag <150829918+mohamad1Aag@users.noreply.github.com>
2025-05-27 22:14:52 +0300

    Update User.js

--- my-backend/models/User.js
+++ my-backend/models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  name:     { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+// ✅ تجنب OverwriteModelError
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
