6c7fc2f5872ea24a5582418ed6854a48d5e60b52
Mohammad <aagmohamad4@gmail.com>
2025-05-30 10:23:20 +0300

    add section

--- my-backend/models/Product.js
+++ my-backend/models/Product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Section = require('../models/Section'); // تأكد من استيراده
+const Section = require('./Section'); // تأكد من استيراده
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
