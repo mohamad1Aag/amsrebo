cefe5ea69ad20e881c7b9c2f9aa9799791a8cc55
Mohammad <aagmohamad4@gmail.com>
2025-05-30 23:47:10 +0300

    add

--- my-backend/routes/captainRoutes.js
+++ my-backend/routes/captainRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Captain = require('../models/Captain');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
