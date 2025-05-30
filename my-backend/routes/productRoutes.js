11bd068dbe0920f9b013e95fd1a49f9d9bfae9cc
Mohammad <aagmohamad4@gmail.com>
2025-05-30 09:49:42 +0300

    add cloudinary

--- my-backend/routes/productRoutes.js
+++ my-backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const upload = require('../middlewares/cloudinaryUpload'); // ✅ هذا هو ملف إعداد Cloudinary
+const upload = require('../middlewares/cloudinary'); // ✅ هذا هو ملف إعداد Cloudinary
 
 // إضافة منتج مع رفع صورة
 router.post('/products', upload.single('image'), productController.createProduct);
