fa964cf65d236c88b8902ddc8e55b8e53ed8db2e
Mohammad <aagmohamad4@gmail.com>
2025-06-17 13:43:49 +0300

    add points

--- my-backend/models/user.js
+++ my-backend/models/user.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name:     { type: String, required: true },
   email:    { type: String, required: true, unique: true },
+  point: {
+    type: Number,
+    required: true,
+    min: [0, 'يجب أن تكون النقاط رقمًا موجبًا'], // ✅ يقبل فقط رقم >= 0
+    unique: true
+  }, 
   password: { type: String },  // مش لازم كلمة مرور لو Google أو Facebook
   googleId: { type: String },  // لتخزين ID من Google
   facebookId: { type: String } // لتخزين ID من Facebook
