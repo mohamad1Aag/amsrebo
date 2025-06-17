5d7fa5fa9b061476b52a8362d587dabbcf2cea0c
Mohammad <aagmohamad4@gmail.com>
2025-06-17 19:21:29 +0300

    add pos

--- my-backend/middlewares/authadminMiddleware.js
+++ my-backend/middlewares/authadminMiddleware.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 const Admin = require('../models/Admin');
 
 const protectAdmin = async (req, res, next) => {
-  const token = req.headers.authorization;
-
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
+  
   if (!token) return res.status(401).json({ message: 'Token not provided' });
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const admin = await Admin.findById(decoded.id).select('-password');
