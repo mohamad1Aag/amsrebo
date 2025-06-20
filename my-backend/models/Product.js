d8886ea9d4d24e7e3a645b297d94df288be4d86f
Mohammad <aagmohamad4@gmail.com>
2025-06-20 03:45:13 +0300

    add order

--- my-backend/models/Product.js
+++ my-backend/models/Product.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-      const productSchema = new mongoose.Schema({
-        name: { type: String, required: true },
-        price: { type: Number, required: true },
-        description: String,
-        section: { type: mongoose.Schema.Types.ObjectId, ref: 'Section', required: true },
-        image: { type: String },
-        adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true }, // ✅  
-        
-      },
- { timestamps: true });
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: String,
+  section: { type: mongoose.Schema.Types.ObjectId, ref: 'Section', required: true },
+  image: { type: String },
+  adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
+}, { timestamps: true });  // <-- تأكد من إغلاق القوس هنا
+
+module.exports = mongoose.model('Product', productSchema);
