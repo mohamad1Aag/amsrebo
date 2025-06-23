9d37b8fb9dac24ecb13a05d820a8931ba2b7cc40
Mohammad <aagmohamad4@gmail.com>
2025-06-24 01:05:57 +0300

    a

--- my-backend/controllers/userController.js
+++ my-backend/controllers/userController.js
@@ -268,7 +268,18 @@ const updateUserWallet = async (req, res) => {
   }
 };
 
-
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 module.exports ={updateUserPoints,
    registerUser,
     loginUser,
@@ -281,7 +292,8 @@ module.exports ={updateUserPoints,
        deleteUser,
        forgotPasswordByUsername,
         getResetPasswordInfo ,
-        updateUserWallet};
+        updateUserWallet,
+        getUserById,};
 
 
 
