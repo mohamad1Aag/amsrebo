8ead56faafbc3a01eb152d0f94f09c16a98c720d
Mohammad <aagmohamad4@gmail.com>
2025-06-27 15:23:43 +0300

    capti

--- my-backend/routes/captainRoutes.js
+++ my-backend/routes/captainRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middlewares/cloudinary');
 const authCaptain = require('../middlewares/authCaptain');
 const captainController = require('../controllers/captainController');
 
