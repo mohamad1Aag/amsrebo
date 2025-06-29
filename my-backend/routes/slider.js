b759eac1fa26f915f140c5d40cb2a9e9170d8b5d
Mohammad <aagmohamad4@gmail.com>
2025-06-29 14:14:33 +0300

    fix all

--- my-backend/routes/slider.js
+++ my-backend/routes/slider.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const upload = require('../middlewares/upload');
 const { uploadSlider, getSliders, deleteSlider } = require('../controllers/sliderController');
 const adminProtect = require('../middlewares/adminProtect');
+const protect = require('../middlewares/protect');
 
-router.post('/upload', adminProtect, upload.single('image'), uploadSlider);
+router.post('/upload', protect, adminProtect, upload.single('image'), uploadSlider);
 router.get('/', getSliders);
 router.delete('/:id',  adminProtect, deleteSlider);
 
