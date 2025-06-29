6070d9549e82fe2d23e0821d2c064efa3141ff1b
Mohammad <aagmohamad4@gmail.com>
2025-06-29 20:32:14 +0300

    capti

--- my-backend/models/Admin.js
+++ my-backend/models/Admin.js
@@ -5,7 +5,7 @@ const adminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role:     { type: String, default: 'admin' } // ✅ حقل الدور
+  role:     { type: String, default: 'miniadmin' } // ✅ حقل الدور
 });
 
 // تشفير كلمة المرور قبل الحفظ
