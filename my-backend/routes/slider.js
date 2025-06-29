fa7cd0c139b46df91c936fdf912f2c4a10d22b8d
Mohammad <aagmohamad4@gmail.com>
2025-06-29 14:19:51 +0300

    fix all

--- my-backend/routes/slider.js
+++ my-backend/routes/slider.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/upload');
+const upload = require('../middlewares/upload'); // multer middleware
 const { uploadSlider, getSliders, deleteSlider } = require('../controllers/sliderController');
-const adminProtect = require('../middlewares/adminProtect');
-const protect = require('../middlewares/protect');
 
-router.post('/upload', protect, adminProtect, upload.single('image'), uploadSlider);
+router.post('/upload', upload.single('image'), uploadSlider);
 router.get('/', getSliders);
-router.delete('/:id',  adminProtect, deleteSlider);
+router.delete('/:id', deleteSlider);
 
 module.exports = router;
