eacdd5ae452df8e3d8e35dbbf85f22daf5dc66ca
Mohammad <aagmohamad4@gmail.com>
2025-06-29 14:45:43 +0300

    fix all

--- my-backend/routes/slider.js
+++ my-backend/routes/slider.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const   { upload } = require('../middlewares/upload'); // multer middleware
+const   upload = require('../middlewares/upload'); // multer middleware
 const { uploadSlider, getSliders, deleteSlider } = require('../controllers/sliderController');
 
 router.post('/upload', upload.single('image'), uploadSlider);
